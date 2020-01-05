a9be4ffed3c56cf47c175ce33cc220e93160daa6
am10 <adventam10@gmail.com>
2020-01-05 15:52:18 +0900

    First commit

---
+++ DNAConverter/app/src/main/java/am10/dnaconverter/DNAConverterModel.kt
@@ -0,0 +1,69 @@
+package am10.dnaconverter
+
+import android.net.Uri
+import java.net.URLEncoder
+
+class DNAConverterModel {
+    var originalText: String? = null
+    var convertedText: String? = null
+    val dnaHexValues = mapOf(
+        "AA" to "0", "AT" to "1", "AC" to "2", "AG" to "3",
+        "TA" to "4", "TT" to "5", "TC" to "6", "TG" to "7",
+        "CA" to "8", "CT" to "9", "CC" to "a", "CG" to "b",
+        "GA" to "c", "GT" to "d", "GC" to "e", "GG" to "f"
+    )
+    val twitterURL: Uri?
+    get() {
+        if (convertedText.isNullOrEmpty()) {
+            return null
+        }
+        val url = "https://twitter.com/intent/tweet?text=" + urlEncode(convertedText!!) + hashTag
+        return Uri.parse(url)
+    }
+    val hashTag: String
+    get() {
+        return "&hashtags=" + urlEncode("DNA変換")
+    }
+    fun urlEncode(text: String): String {
+        return URLEncoder.encode(text, "UTF-8")
+    }
+    fun convertToDNA(text: String?): String {
+        if (text.isNullOrEmpty()) {
+            return "...むり"
+        }
+        val hex = text.toByteArray().map { b -> String.format("%02X", b) }.joinToString("")
+        var result = hex.toLowerCase()
+        dnaHexValues.forEach { (k, v) -> result = result.replace(v, k) }
+        return result
+    }
+
+    fun convertToLanguage(text: String?): String {
+        if (text.isNullOrEmpty()) {
+            return "...むり"
+        }
+        if (isInvalidDNA(text)) {
+            return "...むり"
+        }
+        var index = 0
+        val strings: MutableList<String> = mutableListOf()
+        while (index < text.length) {
+            strings.add(text.substring(index, index+2))
+            index += 2
+        }
+        val hex = strings.map { n -> dnaHexValues[n] }.joinToString("").toUpperCase()
+        val result = ByteArray(hex.length / 2) { hex.substring(it * 2, it * 2 + 2).toInt(16).toByte() }
+        return String(result)
+    }
+
+    fun isInvalidDNA(text: String?): Boolean {
+        if (text.isNullOrEmpty() || text.length%2!=0) {
+            println("a: $text")
+            return true
+        }
+        if (Regex("""[ATCG]+""").matchEntire(text)?.value == null) {
+            println("b: $text")
+            return true
+        }
+        return false
+    }
+}
---
+++ DNAConverter/app/src/main/java/am10/dnaconverter/MainActivity.kt
@@ -0,0 +1,68 @@
+package am10.dnaconverter
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+
+    enum class Mode {
+        LANGUAGE, DNA
+    }
+
+    val convertedTextView: TextView
+    get() {
+        return findViewById<TextView>(R.id.text_view_converted)!!
+    }
+    val originalEditText: EditText
+        get() {
+            return findViewById<EditText>(R.id.edit_text_original)!!
+        }
+    val mode: Mode
+    get() {
+        val radioGroup: RadioGroup = findViewById(R.id.radio_group_mode)
+        if (radioGroup.checkedRadioButtonId == R.id.radio_button_language) {
+            return Mode.LANGUAGE
+        }
+        return Mode.DNA
+    }
+    val model = DNAConverterModel()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val convertButton: Button = findViewById(R.id.button_convert)
+        convertButton.setOnClickListener {
+            hideKeyboard()
+            setTexts()
+            if (mode == Mode.LANGUAGE) {
+                convertedTextView.text = model.convertToDNA(model.originalText)
+            } else {
+                convertedTextView.text = model.convertToLanguage(model.originalText)
+            }
+        }
+        val twitterButton: ImageButton = findViewById(R.id.image_button_twitter)
+        twitterButton.setOnClickListener {
+            hideKeyboard()
+            setTexts()
+            val intent = Intent(Intent.ACTION_VIEW, model.twitterURL)
+            startActivity(intent)
+        }
+    }
+
+    private fun hideKeyboard() {
+        val view = currentFocus
+        if (view != null) {
+            val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            manager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+    private fun setTexts() {
+        model.originalText = originalEditText.text.toString()
+        model.convertedText = convertedTextView.text.toString()
+    }
+
+}
