e01376e48784d9f14d39ab2da3af5015f39f8092
am10 <adventam10@gmail.com>
2020-01-16 21:08:19 +0900

    Refactoring

--- DNAConverter/app/src/main/java/am10/dnaconverter/DNAConverterModel.kt
+++ DNAConverter/app/src/main/java/am10/dnaconverter/DNAConverterModel.kt
@@ -1,27 +1,48 @@
 package am10.dnaconverter
 
 import am10.dnaconverter.Extensions.*
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Environment
+import androidx.core.app.ActivityCompat
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 import java.text.SimpleDateFormat
 import java.util.*
 
+enum class DownloadFileState {
+    COMPLETED, UNWRITABLE, FAILED
+}
 
-class DNAConverterModel {
+interface DownloadFileCallBack {
+    fun onDownloaded(result: DownloadFileState)
+}
+
+class DNAConverterModel(context: Context, private val downloadFileCallBack: DownloadFileCallBack) {
+
+    val REQUEST_PERMISSION = 1000
     var originalText: String? = null
     var convertedText: String? = null
-    val dnaHexValues = mapOf(
+
+    private val dnaHexValues = mapOf(
         "AA" to "0", "AT" to "1", "AC" to "2", "AG" to "3",
         "TA" to "4", "TT" to "5", "TC" to "6", "TG" to "7",
         "CA" to "8", "CT" to "9", "CC" to "a", "CG" to "b",
         "GA" to "c", "GT" to "d", "GC" to "e", "GG" to "f"
     )
+    private val hashTag: String = "&hashtags=" + context.getString(R.string.hash_tag).urlEncode()
 
     fun makeFileName(): String {
         val nowDate = SimpleDateFormat("yyyyMMddHHss").format(Date())
-        return nowDate!! + ".txt"
+        return "$nowDate.txt"
     }
 
-    fun getTwitterURL(hashTag: String): Uri? {
+    fun getTwitterURL(): Uri? {
         if (convertedText.isNullOrEmpty()) {
             return null
         }
@@ -54,5 +75,48 @@ class DNAConverterModel {
             return true
         }
         return false
+    }
+
+    fun isExternalStorageWritable(): Boolean {
+        val state = Environment.getExternalStorageState()
+        return Environment.MEDIA_MOUNTED == state
+    }
+
+    fun isAcceptedExternalStoragePermission(context: Context) : Boolean {
+        return ActivityCompat.checkSelfPermission(context,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun shouldShowRequestExternalStoragePermission(activity: Activity) : Boolean {
+        return ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
+    fun requestExternalStoragePermission(activity: Activity) {
+        val reqPermissions = ArrayList<String>()
+        reqPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(activity,
+            reqPermissions.toTypedArray(), REQUEST_PERMISSION)
+    }
+
+    fun saveFile(context: Context, fileName: String, text: String) {
+        if (!isExternalStorageWritable()) {
+            downloadFileCallBack.onDownloaded(DownloadFileState.UNWRITABLE)
+            return
+        }
+        val path = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        val file = File(path, fileName)
+        try {
+            FileOutputStream(file, true).use{ fileOutputStream ->
+                OutputStreamWriter(fileOutputStream, "UTF-8").use{ outputStreamWriter ->
+                    BufferedWriter(outputStreamWriter).use{ bw ->
+                        bw.write(text+"\n")
+                        bw.flush()
+                    }
+                }
+            }
+            downloadFileCallBack.onDownloaded(DownloadFileState.COMPLETED)
+        } catch (e: Exception) {
+            downloadFileCallBack.onDownloaded(DownloadFileState.FAILED)
+        }
     }
 }
--- DNAConverter/app/src/main/java/am10/dnaconverter/MainActivity.kt
+++ DNAConverter/app/src/main/java/am10/dnaconverter/MainActivity.kt
@@ -1,7 +1,6 @@
 package am10.dnaconverter
 
 import am10.dnaconverter.Extensions.*
-import android.Manifest
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
@@ -10,103 +9,70 @@ import android.os.Bundle
 import android.content.Intent
 import android.view.Menu
 import android.view.MenuItem
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStreamWriter
-import android.os.Environment
 import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+enum class ConvertMode {
+    LANGUAGE, DNA
+}
 
-    enum class Mode {
-        LANGUAGE, DNA
-    }
-    private val hashTag: String
-        get() {
-            return "&hashtags=" + getString(R.string.hash_tag).urlEncode()
-        }
+class MainActivity : AppCompatActivity(), DownloadFileCallBack {
 
-    private val REQUEST_PERMISSION = 1000
-
-    private val mode: Mode
+    private lateinit var model: DNAConverterModel
+    private val mode: ConvertMode
     get() {
         if (radio_group_mode.checkedRadioButtonId == R.id.radio_button_language) {
-            return Mode.LANGUAGE
+            return ConvertMode.LANGUAGE
         }
-        return Mode.DNA
+        return ConvertMode.DNA
     }
-    private val model = DNAConverterModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        model = DNAConverterModel(this, this)
         button_convert.setOnClickListener {
             hideKeyboard()
-            setTexts()
-            if (mode == Mode.LANGUAGE) {
-                text_view_converted.text = model.convertToDNA(model.originalText)?: getString(R.string.error_message)
-            } else {
-                text_view_converted.text = model.convertToLanguage(model.originalText)?: getString(R.string.error_message)
-            }
+            convertDNA()
         }
-
         image_button_twitter.setOnClickListener {
             hideKeyboard()
-            setTexts()
-            if (model.isInvalidDNA(model.convertedText)) {
-                showConfirmationDialog(getString(R.string.alert_title), getString(R.string.alert_message))
-                return@setOnClickListener
-            }
-            val intent = Intent(Intent.ACTION_VIEW, model.getTwitterURL(hashTag))
-            startActivity(intent)
+            tweetDNA()
         }
-
         image_button_download.setOnClickListener {
             hideKeyboard()
-            setTexts()
-            if (model.isInvalidDNA(model.convertedText)) {
-                showConfirmationDialog(getString(R.string.alert_title), getString(R.string.alert_message))
-                return@setOnClickListener
-            }
-            checkPermission()
+            downloadDNA()
         }
-
         image_button_copy.setOnClickListener {
             hideKeyboard()
-            setTexts()
-            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            clipboard.setPrimaryClip(ClipData.newPlainText(getString(R.string.mode_dna), model.convertedText))
-            showShortToast(getString(R.string.copy_message))
+            copyConvertedText()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
-        val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu, menu)
+
+        menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.getItemId()) {
+        return when (item.itemId) {
             R.id.share -> {
                 shareConvertedText()
-                return true
+                true
             }
-            else -> return super.onOptionsItemSelected(item)
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_PERMISSION) {
+        if (requestCode == model.REQUEST_PERMISSION) {
             if (grantResults.isNotEmpty()) {
                 for (i in permissions.indices) {
                     if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                        saveFile(model.makeFileName(), model.convertedText!!)
+                        model.saveFile(this, model.makeFileName(), model.convertedText!!)
                     } else {
                         showShortToast(getString(R.string.file_permission_denied_message))
                     }
@@ -115,18 +81,66 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDownloaded(result: DownloadFileState) {
+        val message = when (result) {
+            DownloadFileState.COMPLETED -> getString(R.string.download_message)
+            DownloadFileState.UNWRITABLE -> getString(R.string.file_not_writable_message)
+            DownloadFileState.FAILED -> getString(R.string.file_download_failed_message)
+        }
+        showShortToast(message)
+    }
+
+    private fun convertDNA() {
+        setTexts()
+        if (mode == ConvertMode.LANGUAGE) {
+            text_view_converted.text = model.convertToDNA(model.originalText)?: getString(R.string.error_message)
+        } else {
+            text_view_converted.text = model.convertToLanguage(model.originalText)?: getString(R.string.error_message)
+        }
+    }
+
+    private fun tweetDNA() {
+        setTexts()
+        if (model.isInvalidDNA(model.convertedText)) {
+            showConfirmationDialog(getString(R.string.alert_title), getString(R.string.alert_message))
+            return
+        }
+        val intent = Intent(Intent.ACTION_VIEW, model.getTwitterURL())
+        startActivity(intent)
+    }
+
+    private fun downloadDNA() {
+        setTexts()
+        if (model.isInvalidDNA(model.convertedText)) {
+            showConfirmationDialog(getString(R.string.alert_title), getString(R.string.alert_message))
+            return
+        }
+        if (model.isAcceptedExternalStoragePermission(this)) {
+            model.saveFile(this, model.makeFileName(), model.convertedText!!)
+        } else {
+            requestExternalStoragePermission()
+        }
+    }
+
+    private fun copyConvertedText() {
+        setTexts()
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.setPrimaryClip(ClipData.newPlainText(getString(R.string.mode_dna), model.convertedText))
+        showShortToast(getString(R.string.copy_message))
+    }
+
     private fun shareConvertedText() {
         setTexts()
         if (model.isInvalidDNA(model.convertedText)) {
             showConfirmationDialog(getString(R.string.alert_title), getString(R.string.alert_message))
             return
         }
+
         val sendIntent: Intent = Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_TEXT, model.convertedText)
             type = "text/plain"
         }
-
         val shareIntent = Intent.createChooser(sendIntent, null)
         startActivity(shareIntent)
     }
@@ -136,60 +150,12 @@ class MainActivity : AppCompatActivity() {
         model.convertedText = text_view_converted.text.toString()
     }
 
-    private fun saveFile(fileName: String, text: String) {
-        if (!isExternalStorageWritable()) {
-            showShortToast(getString(R.string.file_not_writable_message))
-            return
-        }
-        val path = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
-        val file = File(path, fileName)
-        try {
-            FileOutputStream(file, true).use{ fileOutputStream ->
-                OutputStreamWriter(fileOutputStream, "UTF-8").use{ outputStreamWriter ->
-                    BufferedWriter(outputStreamWriter).use{ bw ->
-                        bw.write(text+"\n")
-                        bw.flush()
-                    }
-                }
-            }
-            showShortToast(getString(R.string.download_message))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            showShortToast(getString(R.string.file_download_failed_message))
-        }
-    }
-
-    private fun isExternalStorageWritable(): Boolean {
-        val state = Environment.getExternalStorageState()
-        return Environment.MEDIA_MOUNTED == state
-    }
-
-    private fun checkPermission() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-           saveFile(model.makeFileName(), model.convertedText!!)
-        } else {
-            requestLocationPermission()
-        }
-    }
-
-    private fun requestLocationPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            val reqPermissions = ArrayList<String>()
-            reqPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            ActivityCompat.requestPermissions(this,
-                reqPermissions.toTypedArray(), REQUEST_PERMISSION)
-
+    private fun requestExternalStoragePermission() {
+        if (model.shouldShowRequestExternalStoragePermission(this)) {
+            model.requestExternalStoragePermission(this)
         } else {
             showShortToast(getString(R.string.file_permission_denied_message))
-            val reqPermissions = ArrayList<String>()
-            reqPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            ActivityCompat.requestPermissions(this,
-                reqPermissions.toTypedArray(), REQUEST_PERMISSION)
+            model.requestExternalStoragePermission(this)
         }
     }
 }
